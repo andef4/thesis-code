ff607d6943652f69322eac324f1333f4ecfdb74c
Fabio Anderegg <andef4@bfh.ch>
2019-03-11 11:31:43 +0100

    nhs-chest-xray: update image resize code

--- nhs-chest-xray/resize_image.py
+++ nhs-chest-xray/resize_image.py
@@ -1,9 +1,16 @@
 from PIL import Image
 
-def resize_image(paths):
+
+def resize_image(paths, size):
     in_file, out_file = paths
     image = Image.open(in_file)
     rgb_image = Image.new('RGB', image.size)
     rgb_image.paste(image)
-    rgb_image = rgb_image.resize((299, 299), Image.NEAREST)
+    rgb_image = rgb_image.resize((size, size), Image.NEAREST)
     rgb_image.save(out_file)
+
+def resize_image224(paths):
+    resize_image(paths, 224)
+
+def resize_image299(paths):
+    resize_image(path, 299)
