00c3498c9cfe367c5af60cf4041d6d0b66d64850
Fabio Anderegg <andef4@bfh.ch>
2019-04-04 20:05:32 +0200

    brats: implement hausdorff and dice coefficient in training loop

--- brats/training_loop.py
+++ brats/training_loop.py
@@ -7,6 +7,8 @@ import os
 import torch
 from pathlib import Path
 from datetime import datetime
+from scipy.spatial.distance import directed_hausdorff, dice
+import numpy as np
 
 
 def train_model(name, model, dataloaders, criterion, optimizer, device, num_epochs=25):
@@ -16,6 +18,9 @@ def train_model(name, model, dataloaders, criterion, optimizer, device, num_epoc
     if results_file.exists():
         raise Exception('Result file already exists, please change name')
     f = open(results_file, 'w', buffering=1)
+    f.write('TL: training loss, VL: validation loss\n')
+    f.write('THD: training hausdorf distance, VHD: validation hausdorf distance\n')
+    f.write('TDD: training dice distance, VDD: validation dice distance\n')
 
     for epoch in range(num_epochs):
         epoch_start = datetime.now()
@@ -30,6 +35,8 @@ def train_model(name, model, dataloaders, criterion, optimizer, device, num_epoc
                 model.eval()   # Set model to evaluate mode
 
             running_loss = 0.0
+            hausdorff_distances = []
+            dice_coefficients = []
 
             # Iterate over data.
             for data in dataloaders[phase]:
@@ -56,13 +63,28 @@ def train_model(name, model, dataloaders, criterion, optimizer, device, num_epoc
                 # statistics
                 running_loss += loss.item() * inputs.size(0)
 
+                for i in range(0, len(outputs)):
+                    output = outputs[i].detach().cpu().numpy()
+                    segment = labels[i].detach().cpu().numpy()
+                    dh1 = directed_hausdorff(output, segment)[0]
+                    dh2 = directed_hausdorff(segment, output)[0]
+                    hausdorff_distances.append(np.max([dh1, dh2]))
+                    dice_coefficient = dice(output.reshape(240 * 240), segment.reshape(240 * 240))
+                    dice_coefficients.append(dice_coefficient)
+
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_hausdorff = np.mean(hausdorff_distances)
+            epoch_dice = np.mean(dice_coefficients)
 
             if phase == 'train':
                 train_loss = epoch_loss
+                train_hausdorff = epoch_hausdorff
+                train_dice = epoch_dice
             else:
                 time = datetime.now() - epoch_start
-                stats = f'Epoch: {epoch}, TL: {train_loss:.5f}, VL: {epoch_loss:.5f}, Time: {time}'
+                stats = f'Epoch: {epoch}, TL: {train_loss:.5f}, VL: {epoch_loss:.5f} ' + \
+                        f'THD: {train_hausdorff:.5f}, VHD: {epoch_hausdorff:.5f} ' + \
+                        f'TDD: {train_dice:.5f}, VDD: {epoch_dice:.5f}, Time: {time}'
                 print(stats)
                 f.write(f'{stats}\n')
 
