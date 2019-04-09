1f7c5faec134498af48fea7a699525bac234b2e4
Fabio Anderegg <andef4@bfh.ch>
2019-04-09 17:20:01 +0200

    brats: plot_utils: allow plotting of single images, define color_map used for all images

--- brats/plot_utils.py
+++ brats/plot_utils.py
@@ -1,25 +1,33 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_image_row(images, *, title=None, labels=None, color_maps=None):
+def plot_image_row(images, *, title=None, labels=None, color_map=None, color_maps=None):
     image_count = len(images)
-    assert image_count > 1
 
-    if image_count >= 5:
-        figsize = (20, 4)
-    elif image_count == 4:
-        figsize = (20, 5)
-    elif image_count == 3:
-        figsize = (15, 5)
+    if image_count == 1:
+        figsize = (4, 4)
     elif image_count == 2:
         figsize = (10, 5)
+    elif image_count == 3:
+        figsize = (15, 5)
+    elif image_count == 4:
+        figsize = (20, 5)
+    else:
+        figsize = (20, 4)
 
     figure, plots = plt.subplots(1, len(images), figsize=figsize)
+    # when there is only one subplot, matplotlib returns the plot directly instead of an numpy array
+    if not isinstance(plots, np.ndarray):
+        plots = [plots]
+
     if title:
         figure.suptitle(title, fontsize=16)
 
     for i, plot in enumerate(plots):
-        if color_maps:
+        if color_map:
+            plot.imshow(images[i], cmap=color_map)
+        elif color_maps:
             plot.imshow(images[i], cmap=color_maps[i])
         else:
             plot.imshow(images[i])
