d88a31e9b37a606aabf19a929e2143a9d3b51013
Fabio Anderegg <andef4@bfh.ch>
2019-06-08 14:21:04 +0200

    brats: plot_utils: only allow colorbar with a single image, automatic figsize when only one image

--- brats/plot_utils.py
+++ brats/plot_utils.py
@@ -5,9 +5,11 @@ import numpy as np
 def plot_image_row(images, *, title=None, labels=None, color_map=None, color_maps=None,
                    overlay=None, overlay_alpha=0.8, overlay_color_map='Reds', colorbar=False):
     image_count = len(images)
+    if image_count != 1 and colorbar:
+        raise ValueError('colorbar only works reliable with a single image')
 
     if image_count == 1:
-        figsize = (4, 4)
+        figsize = None
     elif image_count == 2:
         figsize = (10, 5)
     elif image_count == 3:
